d4c319b206fc24f651c6377ad3c3c4a19cf0a88f
Alex Wan <a_wanderous@hotmail.com>
2019-10-19 11:26:55 +1100

    added routes to html

--- server.js
+++ server.js
@@ -4,9 +4,33 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
+});
+
+var reservations = [];
+var waitList = [];
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
+});
+
+app.get("/waitlist", function (req, res) {
+    res.sendFile(path.join(__dirname, "wait.html"));
+});
+
+app.get("/api/tables", function (req, res) {
+    return res.json(reservations);
+});
+app.get("/api/waitinglist", function (req, res) {
+    return res.json(waitList);
+});
